53dc2a77554deb52c8786812d18765091c5286c4
Yizhuang Zhou <zhouyz9608@gmail.com>
2018-08-17 18:12:57 +0800

    add gtest

--- include/tensor.h
+++ include/tensor.h
@@ -4,44 +4,12 @@
 #pragma once
 
 #include <iostream>
-#include <sstream>
 #include <memory>
 
 #include <cuda.h>
 #include <cudnn.h>
 
-// (default) SINGLE GPU
-#define SINGLE_GPU
-
-#define FatalError(s) do {                                           \
-  std::stringstream _where, _message;                                \
-  _where << __FILE__ << ':' << __LINE__;                             \
-  _message << std::string(s) + "\n" << __FILE__ << ':' << __LINE__;  \
-  std::cerr << _message.str() << "\nAborting...\n";                  \
-  cudaDeviceReset();                                                 \
-  exit(1);                                                           \
-} while(0)
-
-#define CHECK_CUDNN(status) do {                                     \
-  std::stringstream _error;                                          \
-  if (status != CUDNN_STATUS_SUCCESS) {                              \
-    _error << "CUDNN failure: " << cudnnGetErrorString(status);      \
-    FatalError(_error.str());                                        \
-  }                                                                  \
-} while(0)
-
-#define CHECK_CUDA(status) do {                                      \
-  std::stringstream _error;                                          \
-  if (status != 0) {                                                 \
-    _error << "Cuda failure: " << cudaGetErrorString(status);        \
-    FatalError(_error.str());                                        \
-  }                                                                  \
-} while(0)
-
-#define DISABLE_ASSIGN_COPY(Class)                                   \
-  Class(const Class&) = delete;                                      \
-  Class& operator=(const Class&) = delete;                           \
-
+#include "common.h"
 
 /* Synced Memory between CPU and GPU */
 class SyncedMemory {
---
+++ test/zero_test.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+TEST(Zero, ZeroTest) {
+  ASSERT_EQ(0, 0);
+}
